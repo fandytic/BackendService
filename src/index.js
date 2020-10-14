9dd6d6b8118682e2271e28fc9e6a66dd531765e6
Fandy Hidayat <fandyh97@gmail.com>
2020-10-14 09:41:08 +0700

    deploy

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const server = require('./app');
+const port = process.env.PORT || 3000
 
-server.listen(process.env.PORT, () => {
-  console.log('Server running on port', process.env.PORT);
+server.listen(port, () => {
+  console.log('Server running on port', port);
 });
